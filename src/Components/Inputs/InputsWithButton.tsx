a691e7dca7d3eb06bcc2da89bb03ee6b5457df92
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-07 15:07:57 +0100

    Input with button component

---
+++ src/Components/Inputs/InputsWithButton.tsx
@@ -0,0 +1,41 @@
+import React, { ChangeEvent, useState } from "react";
+
+interface InputsWithButtonProps {
+  onButtonClick(resources: number): void;
+  buttonText: string;
+  inputType: string;
+  inputPlaceHolder: string;
+  inputName: string;
+}
+
+export const InputsWithButton: React.FC<InputsWithButtonProps> = ({
+  onButtonClick,
+  buttonText,
+  inputType,
+  inputPlaceHolder,
+  inputName,
+}) => {
+  const [inputValue, setInputValue] = useState<any>(0);
+
+  const updateInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    onButtonClick(inputValue);
+    setInputValue(0);
+  };
+
+  return (
+    <div>
+      <input
+        onChange={updateInput}
+        value={inputValue}
+        type={inputType}
+        name={inputName}
+        placeholder={inputPlaceHolder}
+      />
+      <button onClick={handleButtonClick}> {buttonText} </button>
+    </div>
+  );
+};
