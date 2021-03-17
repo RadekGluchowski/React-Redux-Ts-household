a357b56b010824ef7556f9f2bd1e3120dabf134d
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-17 17:58:02 +0100

    added investment reducer

---
+++ src/Store/Reducers/InvestmentReducer/investment.reducer.ts
@@ -0,0 +1,26 @@
+import { ADD_INVESTMENT, InvestmentActions, DONE_INVESTMENT } from "../../Actions/InvestmentActions/investment.actions"
+import produce, { Draft } from 'immer';
+
+export interface InvestmentState {
+    investment: Array<object>
+}
+
+const initialState = {
+    investment: []
+}
+
+export const goalReducer = (state: InvestmentState = initialState, action: InvestmentActions) => {
+    return produce(state, (draft: Draft<InvestmentState>) => {
+        switch (action.type) {
+            case ADD_INVESTMENT:
+                draft.investment = [...draft.investment, action.payload];
+                break;
+            case DONE_INVESTMENT:
+
+                break;
+            default:
+                return draft;
+        }
+    })
+}
+
