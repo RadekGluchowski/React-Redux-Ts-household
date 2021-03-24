1e9b5ec7a4a104168c7b73f5decbaaa8d201cf46
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-24 12:10:38 +0100

    create investment selector

--- src/Store/Selectors/Selectors.ts
+++ src/Store/Selectors/Selectors.ts
@@ -1,10 +1,12 @@
 import { createSelector } from "reselect"
 import { Budget } from "../../interfaces/budget.interface";
 import { GoalsState } from "../Reducers/GoalsReducer/goals.reducer";
+import { InvestmentState } from "../Reducers/InvestmentReducer/investment.reducer";
 import { AppState } from "../Reducers/root-reducer";
 
 const selectGoalsState = (state: AppState) => state.goalReducer;
 const selectBudgetState = (state: AppState) => state.budgetReducer;
+const selectInvestmentState = (state: AppState) => state.investmentReducer;
 
 export const selectGoals = createSelector(
     selectGoalsState,
@@ -14,4 +16,9 @@ export const selectGoals = createSelector(
 export const selectBudget = createSelector(
     selectBudgetState,
     (state: Budget) => state.resources
+)
+
+export const selectInvestment = createSelector(
+    selectInvestmentState,
+    (state: InvestmentState) => state
 )
