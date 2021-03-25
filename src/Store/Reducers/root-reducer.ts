1862a56c45d68fc66e221070e517e11762889e99
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-25 19:01:28 +0100

    added history reducer to root

--- src/Store/Reducers/root-reducer.ts
+++ src/Store/Reducers/root-reducer.ts
@@ -2,12 +2,14 @@
 import { combineReducers } from 'redux';
 import { budgetReducer } from './BudgetReducer/budget.reducer';
 import { goalReducer } from './GoalsReducer/goals.reducer';
+import { historyReducer } from './HistoryReducer/history.reducer';
 import { investmentReducer } from './InvestmentReducer/investment.reducer';
 
 export const rootReducer = combineReducers({
     budget: budgetReducer,
     goals: goalReducer,
-    investment: investmentReducer
+    investment: investmentReducer,
+    history: historyReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
