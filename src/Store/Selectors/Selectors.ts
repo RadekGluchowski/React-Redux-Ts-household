e561849705f5615c5cc0877066871095aac29594
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-25 19:02:46 +0100

    created history selector

--- src/Store/Selectors/Selectors.ts
+++ src/Store/Selectors/Selectors.ts
@@ -1,12 +1,14 @@
 import { createSelector } from "reselect"
 import { Budget } from "../../interfaces/budget.interface";
 import { Goals } from "../../interfaces/goal.interface";
+import { History } from "../../interfaces/history.interface";
 import { Investment } from "../../interfaces/investment.interface";
 import { AppState } from "../Reducers/root-reducer";
 
 const selectGoalsState = (state: AppState) => state.goals;
 const selectBudgetState = (state: AppState) => state.budget;
 const selectInvestmentState = (state: AppState) => state.investment;
+const selectHistoryState = (state: AppState) => state.history;
 
 export const selectGoals = createSelector(
     selectGoalsState,
@@ -21,4 +23,9 @@ export const selectBudget = createSelector(
 export const selectInvestment = createSelector(
     selectInvestmentState,
     (state: Investment) => state
+)
+
+export const selectHistoryEvents = createSelector(
+    selectHistoryState,
+    (state: History) => state
 )
