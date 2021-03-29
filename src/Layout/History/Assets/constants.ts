b13a56b6d9696f379b5ff87508d5f36380293582
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-29 14:43:12 +0200

    added constants for history screen

---
+++ src/Layout/History/Assets/constants.ts
@@ -0,0 +1,43 @@
+export const TYPES_OF_OPERATIONS = {
+    SUBTRACT_FROM_BUDGET: "SUBTRACT_FROM_BUDGET",
+    ADD_TO_BUDGET: "ADD_TO_BUDGET",
+    ADD_GOAL: "ADD_GOAL",
+    CHARGE_GOAL: "CHARGE_GOAL",
+    DONE_GOAL: "DONE_GOAL",
+    ADD_INVESTMENT: "ADD_INVESTMENT",
+    DONE_INVESTMENT: "DONE_INVESTMENT"
+}
+
+export const OPERATIONS_DICTIONARY = {
+    SUBTRACT_FROM_BUDGET: "Subtracted from Your budget",
+    ADD_TO_BUDGET: "Added to Your Budget",
+    ADD_GOAL: "New Goal was added",
+    CHARGE_GOAL: "Charged piece of goal",
+    DONE_GOAL: "Goal is done",
+    ADD_INVESTMENT: "Added new investment",
+    DONE_INVESTMENT: "Investment is done"
+}
+
+export const TYPES_OF_FIELDS = {
+    GOAL_NEEDED_RESOURCES: "goalNeededResources",
+    GOAL_DESCRITPION: "goalDescription",
+    GOAL_EDIT_CHARGE: "charge",
+    GOAL_EDIT_LINE: "goalIndex",
+    RESOURCES: "resources",
+    TYPE_OF_INVESTMENT: "typeOfInvestment",
+    INVESTMENT_AMOUNT: "investmentAmount"
+}
+
+export const FIELDS_DICTIONARY = {
+    GOAL_NEEDED_RESOURCES: "Goal Resources that's are needed",
+    GOAL_DESCRITPION: "Goal Description",
+    GOAL_EDIT_CHARGE: "Amount of charged",
+    GOAL_EDIT_LINE: "Goal with index: ",
+    RESOURCES: "Resources ",
+    TYPE_OF_INVESTMENT: "Type of investment",
+    INVESTMENT_AMOUNT: "Investment Amount"
+}
+
+export const OPERATION_TIME_STRING = "Operation Time: "
+export const OPERATION = "Operation: "
+export const WITH_DATA = "With data: "
