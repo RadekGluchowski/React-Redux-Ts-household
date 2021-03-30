608b593a3b826d65c146cbc7a29a4b73780a969a
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-30 20:03:19 +0200

    added constants for top nav bar

---
+++ src/Components/TopNavBar/Assets/constants.ts
@@ -0,0 +1,27 @@
+export const DASHBOARD = "Dashboard"
+export const GOALS = "Goals";
+export const INVESTMENT = "Investment";
+export const HISTORY = "History";
+
+export const SidebarData = [
+    {
+        title: DASHBOARD,
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        title: GOALS,
+        path: '/Goals',
+        cName: 'nav-text'
+    },
+    {
+        title: INVESTMENT,
+        path: '/Investment',
+        cName: 'nav-text'
+    },
+    {
+        title: HISTORY,
+        path: '/HistoryScreen',
+        cName: 'nav-text'
+    }
+]
