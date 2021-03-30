fb623fe21db5af3215f06220ff36fd4cbdc6a47c
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-30 18:17:46 +0200

    basic css fro whole app

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="app">
           <TopNavBar />
           <RunningInvestments />
           <Switch>
