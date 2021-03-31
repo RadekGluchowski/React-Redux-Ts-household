3b9ff02afe80d54be6df5585401371cd8abb3d12
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-03-31 20:18:11 +0200

    add constants for footer

---
+++ src/Components/Footer/Assets/constants.ts
@@ -0,0 +1,3 @@
+export const CREATED_BY = "Created By:";
+export const AUTOR = "Radosław Głuchowski";
+export const AUTOR_GITHUB_LINK = "https://github.com/RadekGluchowski";
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import './Footer.css'
+import { CREATED_BY, AUTOR_GITHUB_LINK, AUTOR } from './Assets/constants'
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      &copy; {new Date().getFullYear()} Created By:
-      <a href="https://github.com/RadekGluchowski"> Radosław Głuchowski </a>
+      &copy; {new Date().getFullYear()} {CREATED_BY}
+      <a href={AUTOR_GITHUB_LINK}> {AUTOR}  </a>
     </div>
   );
 };
