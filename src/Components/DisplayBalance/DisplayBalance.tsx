cb26ed05d68737e503c5512385bb0dc7e3e9d6d0
radoslawgluchowski <radoslawgluchowski_1@wp.pl>
2021-04-03 14:43:39 +0200

    add constants for display balance

--- src/Components/DisplayBalance/DisplayBalance.tsx
+++ src/Components/DisplayBalance/DisplayBalance.tsx
@@ -3,13 +3,14 @@ import { Budget } from "../../interfaces/budget.interface";
 import { AppState } from "../../Store/Reducers/root-reducer";
 import {selectBudget} from "../../Store/Selectors/Selectors";
 import React from "react";
+import {BALANCE} from "./Assets/constants";
 
 function DisplayBalance() {
   const resources = useSelector<AppState, Budget["resources"]>(selectBudget);
 
   return (
     <div>
-        <span>Balance: {resources}</span>
+        <span> {BALANCE} {resources}</span>
     </div>
   );
 }
