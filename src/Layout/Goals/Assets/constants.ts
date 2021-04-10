5b3d4a1b8a0d6678f32dd939c2efc4a5bade7f61
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2021-04-10 21:49:41 +0100

    revert edit goals btn strings.

--- src/Layout/Goals/Assets/constants.ts
+++ src/Layout/Goals/Assets/constants.ts
@@ -1,8 +1,8 @@
 export const EDIT_GOAL = {
     MODAL_CONTENT_LABEL: "Edit Goal",
-    DONE_GOAL: "Done",
+    DONE_GOAL: "Done Goal",
     CHARGE_BTN: {
-        BUTTON_TEXT: "Spend",
+        BUTTON_TEXT: "Spend on goal",
         INPUT_NAME: "Spend on goal",
         INPUT_TYPE: "number",
         INPUT_PLACEHOLDER: "150..."
